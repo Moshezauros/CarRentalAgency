a4c81a53aebbfa1c0e9820c8cb4ace19b6ba8ea6
Moshezauros <tepper.moshe@gmail.com>
2017-02-26 03:46:02 +0200

    Add files via upload

---
+++ CRA.Entities/GeneralResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRA.Entities
+{
+    public class GeneralResponse
+    {
+        public GeneralResponse()
+        {
+            Errors = new List<string>();
+        }
+
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ CRA.Entities/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRA.Entities
+{
+    public interface IUnitOfWork
+    {
+        void Create<T>(T entity) where T : class;
+        IEnumerable<T> Get<T>() where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void Save();
+    }
+}
